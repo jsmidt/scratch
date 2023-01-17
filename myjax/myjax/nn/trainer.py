1de7e1fd5f41957b095d6196b92e6a9df7bdd494
Joseph Smidt <josephsmidt@gmail.com>
2023-01-17 13:09:29 -0700

    Add valication step capability

--- myjax/myjax/nn/trainer.py
+++ myjax/myjax/nn/trainer.py
@@ -17,25 +17,27 @@ class Trainer:
         # self.fig = None
         self.max_epochs = max_epochs
 
-    def fit(self, params, mymodel, dataloader, log_epoch=1):
+    def fit(self, params, mymodel, train_dataloader, validation_dataloader=None, log_epoch=1, plots=False, plot_log=False):
         '''Train the model by fitting to data'''
 
-        #self.fig = plt.figure()
-        #self.axes = plt.gca()
+        if plots == True:
+            self.fig = plt.figure()
+            self.axes = plt.gca()
         optimizer = mymodel.configure_optimizers()
 
         # Print loss from first batch
-        batch = dataloader[0]
+        batch = train_dataloader[0]
         params, loss = self.fit_step(params, mymodel, optimizer, batch)
         print(
             f'\nEpoch ({0:>5d}/{self.max_epochs:5d}) train_loss = {loss.item():.5g}')
 
         # Now train over each epoch
         n_epoch = [0]
-        n_loss = [loss]
+        n_train_loss = [loss]
+        n_val_loss = [loss]
         for epoch in range(1, self.max_epochs+1):
             eloss = []
-            for step, batch in enumerate(dataloader):
+            for step, batch in enumerate(train_dataloader):
 
                 # Get updated params and loss
                 params, loss = self.fit_step(params, mymodel, optimizer, batch)
@@ -44,18 +46,31 @@ class Trainer:
                 eloss.append(loss)
 
             if epoch % log_epoch == 0:
-                if step > 100000000:
+                if step > 10000000:
                     pass
                 n_epoch.append(epoch)
-                n_loss.append(jnp.mean(jnp.array(eloss)).item())
-                print(
-                    f'Epoch ({epoch:>5d}/{self.max_epochs:5d}) train_loss = {n_loss[-1]:.5g}')
+                n_train_loss.append(jnp.mean(jnp.array(eloss)).item())
+                epoch_str = f'Epoch ({epoch:>5d}/{self.max_epochs:5d}) train_loss = {n_train_loss[-1]:.5f} '
 
-        # Plot output
-        #self.axes.plot(n_epoch, n_loss, 'C0', label='Training, Loss')
-        #self.axes.set_xlabel('Epochs')
-        #self.axes.set_ylabel('Loss')
-        #self.axes.legend()
+                if validation_dataloader is not None:
+                    for bbatch in validation_dataloader:
+                        loss = mymodel.validation_step(params, bbatch)
+                    n_val_loss.append(loss)
+                    epoch_str += f'val_loss = {n_val_loss[-1]:.5f}'
+
+                print(epoch_str)
+
+        if plots == True:
+            # Plot output
+            if plot_log == True:
+                self.axes.semilogy(n_epoch, n_train_loss, 'C0', label='Training, Loss')
+                self.axes.semilogy(n_epoch, n_val_loss, 'C1', label='Validation, Loss')
+            else:
+                self.axes.plot(n_epoch, n_train_loss, 'C0', label='Training, Loss')
+                self.axes.plot(n_epoch, n_val_loss, 'C1', label='Validation, Loss')
+            self.axes.set_xlabel('Epochs')
+            self.axes.set_ylabel('Loss')
+            self.axes.legend()
         return params
 
     def fit_step(self, params, mymodel, optimizer, batch):
--- myjax/myjax/utils/data.py
+++ myjax/myjax/utils/data.py
@@ -45,3 +45,32 @@ class DataLoader:
     def __getitem__(self, idx: int) -> tuple[jnp.array, jnp.array]:
         return self.dataset[idx]
 
+def random_split(dataset, train_length=0.9, key=None):
+    # Get length of dataset
+    len_data = len(dataset)
+
+    # Get length of desired train data
+    if train_length <= 1.0:
+        n_train = int(train_length*len_data)
+    else:
+        n_train = train_length
+
+    # Two sets of depending on whether random key present
+    if key is not None:
+        key, i_key = jax.random.split(key)
+        indices = jax.random.permutation(i_key, len(dataset))
+        train_indicies = indices[:n_train]
+        val_indicies = indices[n_train:]
+    else:
+        indices = jnp.arange(len(dataset))
+        train_indicies = indices[:n_train]
+        val_indicies = indices[n_train:]
+
+    train_X, train_y = dataset[train_indicies]
+    val_X, val_y = dataset[val_indicies]
+
+    train_data = Dataset(train_X, train_y)
+    val_data = Dataset(val_X, val_y)
+
+    return train_data, val_data
+
